0cd1a239f78a064e56c4d1c199afca528bd53be3
zhaohaizhu <zhaohaizhu@xianglin.cn>
2024-05-20 09:49:30 +0800

    feat: 增加全局状态库redux

--- webpack/conf.js
+++ webpack/conf.js
@@ -4,7 +4,7 @@ const PROJECT_PATH = path.resolve(__dirname, '../');
 const PROJECT_NAME = path.parse(PROJECT_PATH).name;
 
 // Dev server host and port
-const SERVER_HOST = '0.0.0.0';
+const SERVER_HOST = 'localhost';
 const SERVER_PORT = 9000;
 
 // Whether to enable bundle package analysis
