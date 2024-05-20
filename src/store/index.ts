79d3a9a3ab1035e4ce0712ad7aa9902c78906fc5
zhaohaizhu <1457586680@qq.com>
2024-05-20 14:15:04 +0800

    feat: dev

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice.js'
+import counterReducer  from './features/counter/counterSlice.js'
 
 export default configureStore({
   reducer: {
